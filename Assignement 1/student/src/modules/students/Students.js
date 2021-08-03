a9d40650a4ddb2f43f5bc0a3ed544fb2538b4948
mirzazaman <mirzazaman828@gmail.com>
2021-08-03 22:21:47 +0500

    Header Added!

--- Assignement 1/student/src/modules/students/Students.js
+++ Assignement 1/student/src/modules/students/Students.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import Header from '../../components/header/Header'
 import useStudents from './useStudents'
+
 
 export default function Students() {
     const [] = useStudents()
     
     return (
         <div>
-            Hello World!
+            <Header/>
         </div>
     )
 }
