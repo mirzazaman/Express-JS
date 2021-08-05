ba3175bc58d197050ea97f6bd05747e5385a55c7
mirzazaman <mirzazaman828@gmail.com>
2021-08-05 13:04:08 +0500

    cors and bodyparse installed

--- Assignement 1/server/app.js
+++ Assignement 1/server/app.js
@@ -1,11 +1,24 @@
 const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
+
 
 app.get('/', (req, res)=>{
     res.send("Hello World!")
 })
 
-app.listen("5000", ()=>{
+app.post('/', (req, res)=>{
+    
+    // console.log(req.body);
+    res.send(`Name is ${req.body.name}`)
+})
+
+app.listen(5000, ()=>{
     console.log("Server runing on port 5000");
 })
