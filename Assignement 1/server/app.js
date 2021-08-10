70f82e5f4ea2432da78dc9485f031a1ae6815ae7
mirzazaman <mirzazaman828@gmail.com>
2021-08-10 17:51:35 +0500

    Some Fix!
    Delete and Update not working

--- Assignement 1/server/app.js
+++ Assignement 1/server/app.js
@@ -15,9 +15,28 @@ app.get('/', (req, res)=>{
 })
 
 app.post('/', (req, res)=>{
-    List.push(req.body)
+    List.push(req.body.data)
     res.send("Data Added")
 })
+
+app.put('/', (req, res)=>{
+    // let data = req.body.data
+    // List.unshift(data)
+    // List.map((item)=>{
+    //     if(data.id === item.id){
+    //         console.log("condition true");
+    //         return data
+
+    //     } else {
+    //         return item
+    //     }
+    // })
+})
+
+app.delete('/', (req, res)=>{
+    console.log("Delete Request", req.body);
+})
+
 
 app.listen(5000, ()=>{
     console.log("Server runing on port 5000");
