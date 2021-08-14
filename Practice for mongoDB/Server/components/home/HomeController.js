51fb2a2945dd05e4d845ba4bea74d03268b5191b
mirzazaman <mirzazaman828@gmail.com>
2021-08-14 22:05:44 +0500

    Format Document

--- Practice for mongoDB/Server/components/home/HomeController.js
+++ Practice for mongoDB/Server/components/home/HomeController.js
@@ -1,14 +1,14 @@
-module.exports.test = (req, res)=>{
+module.exports.test = (req, res) => {
     res.status(200)
     res.json("Function is working")
 }
 
-module.exports.get = (req, res)=>{
+module.exports.get = (req, res) => {
     res.status(200)
     res.json("Get Function is working")
 }
 
-module.exports.post = (req, res)=>{
+module.exports.post = (req, res) => {
     res.status(200)
     res.json(`My name is ${req.body.name}`)
 }
